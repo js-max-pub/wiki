96a7edab0d9015947ac69b5fdb930cfa63bf2400
Maurice Heine <maurice.heine@uni-muenster.de>
2021-03-15 15:47:43 +0100

    Add parsing for multiple atc codes in one line

--- src/Page.js
+++ src/Page.js
@@ -32,32 +32,86 @@ export default class WikiPage {
 
 	async box(boxName, options = {}) {
 		let text = await this.text();
-		if(typeof(text) == "undefined") return false;
-		let start = text.search('{{' + boxName + '\n')
-		// console.log('st',start)
-		if (start == -1) return false;
-		let end = text.search('\n}}\n')
-		let lines = text.slice(start, end).split('\n').slice(1)
-		let output = {}
-		let key, val;
-		for (let line of lines) {
-			if (line.startsWith('|')) { // lines with a key
-				key = line.split('=')[0].slice(1).trim()
-				// if (options.camelCaseKeys) key = (await import('./string.mjs')).toCamelCase(key)
-				if (options.camelCaseKeys) key = camelCase(key)
-				// if (options.lowerCaseKeys) key = key.toLowerCase()
-				// if (options.alphaNumericalKeys) key = key.replace(/[^a-z]/gi, '_')
-				val = line.split('=').slice(1).join('=').trim()
-				if (val.startsWith('*')) // multi-line-entry
-					output[key] = [val.slice(1).trim()]
-				else output[key] = options.onlyArrays ? [val] : val;
-			}
-			if (line.startsWith('*')) { // continuing previous line
-				val = line.slice(1).trim()
-				output[key].push(val)
-			}
-			// if(options.parseValues) 
-		}
+		let output = boxFunction(text,boxName,options)	
 		return output
 	}
+
 }
+function boxFunction(text,boxName,options ={}){
+	if(typeof(text) == "undefined") return false;
+	let start = text.search('{{' + boxName + '\n')
+	// console.log('st',start)
+	if (start == -1) return false;
+	let end = text.search('\n}}\n')
+	let lines = text.slice(start, end).split('\n').slice(1)
+	let output = {}
+	let key, val,brackets;
+	let isParsingBrackets =false
+	for (let line of lines) {
+		if (line.startsWith('|')) { // lines with a key
+			key = line.split('=')[0].slice(1).trim()
+			isParsingBrackets = key=="ATC-Code"
+			// if (options.camelCaseKeys) key = (await import('./string.mjs')).toCamelCase(key)
+			if (options.camelCaseKeys) key = camelCase(key)
+			// if (options.lowerCaseKeys) key = key.toLowerCase()
+			// if (options.alphaNumericalKeys) key = key.replace(/[^a-z]/gi, '_')
+			val = line.split('=').slice(1).join('=').trim()
+			brackets = null;
+			if (val.startsWith('*')){ // multi-line-entry
+				val = val.slice(1).trim()
+				brackets = isParsingBrackets? parseBrackets(val): [val]
+				output[key] = [...brackets]
+			}
+			else {
+				brackets = isParsingBrackets? parseBrackets(val): [val]
+				//array with length > 1 -> array else -> array[0] (val but no array)
+				output[key] = options.onlyArrays ? brackets : brackets.length>1?brackets:brackets[0];
+			}
+		}
+		if (line.startsWith('*')) { // continuing previous line
+			val = line.slice(1).trim()
+			brackets = null;
+			brackets = parseBrackets(val)
+			brackets.forEach(val => output[key].push(val))
+		}
+		// if(options.parseValues) 
+	}
+	//filter array so that empty strings will be removed (removes number 0 too  or everything that js considers to be false)
+	for(key in output){
+		if(output[key]!= null || output[key].length){
+			output[key] = output[key].filter(x=>x)
+		}
+		
+	}
+	//let newParse = parseBox(text)
+//	console.log(newParse)
+return output
+}
+
+//return only found bracketed expression like [{{ATC|S01|AA03}},...] or [text] if no expressions found
+function parseBrackets(text){
+	//const bracketRegex = /{{[^{}]*?}}|\[\[[^\[\]]*?\]\]/gm;
+	//const refRegex = /<ref(?:(?!<ref).)*?(?:\/>|<\/ref>)/gm; regex for detecting references
+	const bracketRegex = /{{[^{}]*?}}/gm;
+	let res = []
+	let m = null;
+	while ((m = bracketRegex.exec(text)) !== null) {
+		if(m==null){
+			continue;
+		}
+		// This is necessary to avoid infinite loops with zero-width matches
+		if (m.index === bracketRegex.lastIndex) {
+			bracketRegex.lastIndex++;
+		}
+		// The result can be accessed through the `m`-variable.
+		m.forEach((match, groupIndex) => {
+			res.push(match)
+			//console.log(`Found match, group ${groupIndex}: ${match}`);
+		});	
+	}
+	if(res.length<1)
+		return [text]
+	else
+		return res;
+}
+
